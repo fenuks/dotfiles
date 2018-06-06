a387ae7a7a373410f1bb01dbe4f48f27455e265e
fenuks <fenuks@protonmail.com>
2018-06-06 03:35:20 +0200

    Add fzf completion for hg (WIP)

---
+++ .config/fzf/completions/fzf-hg.sh
@@ -0,0 +1,58 @@
+#!/bin/env bash
+
+_fzf_complete_hg() {
+    local selected fzf fzf_opt hg hg_opt
+    if [[ -n "${COMP_WORDS[${COMP_CWORD}]}" ]]; then
+        _hg "${@}"
+        return 0
+    fi
+
+    hg="${COMP_WORDS[0]}"
+    hg_opt="${COMP_WORDS[*]:1}"
+    hg_last_opt="${COMP_WORDS[${COMP_CWORD}-1]}"
+    echo "LAST WORD: ${hg_last_opt}"
+    fzf="$(__fzfcmd_complete)"
+    fzf_opt="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse ${FZF_DEFAULT_OPTS} --preview 'echo {}' --preview-window down:3:wrap ${FZF_COMPLETION_OPTS}"
+
+    if [[ "${hg_opt}" == 'merge '* ]] || [[ "${hg_opt}" == 'up '* ]] \
+        || [[ "${hg_opt}" == 'update '* ]] || [[ "${hg_opt}" == 'co '* ]] \
+        || [[ "${hg_opt}" == 'checkout '* ]];
+    then
+        if [[ "${hg_last_opt}" == '-r' ]]; then
+            # TODO: support for revisions
+            _hg "$@"
+            return 0
+        else
+            selected=$( ( ${hg} branches & ${hg} tags ) | FZF_DEFAULT_OPTS=${fzf_opt} ${fzf} -m | awk '{print $1}' | tr '\n' ' ')
+        fi
+    elif [[ "${hg_opt}" == 'add '* ]]; then
+        selected=$( ( ${hg} status --unknown ) | FZF_DEFAULT_OPTS=${fzf_opt} ${fzf} -m | awk '{print $2}' | tr '\n' ' ')
+    elif [[ "${hg_opt}" == 'annotate '* ]]; then
+        selected=$( ( ${hg} files ) | FZF_DEFAULT_OPTS=${fzf_opt} ${fzf} -m | tr '\n' ' ')
+    elif [[ "${hg_opt}" == 'push '* ]]; then
+        if [[ "${hg_last_opt}" == '-r' ]]; then
+            # TODO: support for revisions -r
+            _hg "$@"
+            return 0
+        elif [[ "${hg_last_opt}" == '-b' ]]; then
+            selected=$( ( ${hg} branches ) | FZF_DEFAULT_OPTS=${fzf_opt} ${fzf} -m | tr '\n' ' ')
+        elif [[ "${hg_last_opt}" == '-B' ]]; then
+            # TODO: support for bookmarks
+            _hg "$@"
+            return 0
+        fi
+    elif [[ "${hg_opt}" == 'revert '* ]]; then
+        selected=$( ( ${hg} status -mard ) | FZF_DEFAULT_OPTS=${fzf_opt} ${fzf} -m | awk '{print $2}' | tr '\n' ' ')
+    fi
+
+    if [[ -v selected ]]; then
+        printf '\e[5n'
+    fi
+    if [ -n "${selected}" ]; then
+        COMPREPLY=( "$selected" )
+        return 0
+    fi
+    _hg "$@"
+}
+
+[ -n "$BASH" ] && complete -F _fzf_complete_hg -o default -o bashdefault -o nospace hg
