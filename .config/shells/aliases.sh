2a5f932ed93957214d8a3e21cd76ca7c24d1febd
fenuks <fenuks@o2.pl>
2025-04-15 22:18:42 +0200

    Move aliases that are not compatible with fish to aliases.sh

--- .config/shells/aliases.sh
+++ .config/shells/aliases.sh
@@ -11,6 +11,33 @@ alias vr='vim $(find_up requirements.txt)'
 alias vt='vim $(find . -type f -exec grep -Iq . {} \; -print)'
 alias vt.='vim $(find . -type f -maxdepth 1 -exec grep -Iq . {} \; -print)'
 
+alias cdc="cd ${XDG_CACHE_DIR:-${HOME}/.cache}"
+alias cdd="cd ${XDG_DOCUMENTS_DIR:-${HOME}/Documents}"
+alias cdf="cd ${XDG_VIDEOS_DIR:-${HOME}/Videos}"
+alias cdg="cd ${HOME}/Gry"
+alias cdk="cd ${HOME}/Kod"
+alias cdm="cd ${XDG_MUSIC_DIR:-${HOME}/Music}"
+alias cdn="cd ${XDG_DOCUMENTS_DIR:-${HOME}/Documents}/notatki"
+alias cdp="cd ${XDG_DOWNLOAD_DIR:-${HOME}/Downloads}"
+alias edit="${EDITOR}"
+alias kitty-themes="cd ${HOME}/.config/kitty/themes/ && ls *.conf | fzf --preview 'head -n 40 {} && kitty @ set-colors -a -c {}'; cd -"
+
 if command -v delta >/dev/null; then
   export GIT_PAGER=delta
+fi
+
+if command -v /usr/lib/jvm/default/bin/jshell >/dev/null; then
+  alias jshell="/usr/lib/jvm/default/bin/jshell"
+fi
+
+if command -v exa >/dev/null; then
+  alias l='exa'
+elif command -v lsd >/dev/null; then
+  alias l='lsd'
+else
+  alias l='ls --color --classify --human-readable'
+fi
+
+if ! command -v mutt >/dev/null; then
+  alias mutt='neomutt'
 fi
